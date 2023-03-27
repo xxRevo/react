0e15b83c41cbed9933ac37e470f41efc82db8e98
Revo <berkem_elgul@hotmail.com>
2023-03-27 16:49:31 +0300

    added functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 async function digestMessage(message) {
   const msgUint8 = new TextEncoder().encode(message); 
@@ -11,30 +13,62 @@ async function digestMessage(message) {
   return hashHex;
 }
 
-async function validatePassword(password) {
-  return true;
+function validatePassword(password) {
+  let has_numerical = false;
+  let has_upper_case = false;
+  let char_count = 0;
+
+  for (let i = 0; i < password.length; i++) {
+    let digit = password[i];
+    if (Number.isInteger(parseInt(digit))) {
+      has_numerical = true;
+    }
+    else if (digit === digit.toUpperCase()) {
+      has_upper_case = true;
+    }
+    char_count++;
+  }
+  console.log(has_numerical, " ", has_upper_case, " ", char_count);
+  if ((has_numerical && has_upper_case) && (char_count >= 5)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-function register_function(event) {
-    event.preventDefault(); // REMOVES PAGE REFRESH ON BUTTON CLICK
+function register_function() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var password2 = document.getElementById("password2").value;
 
     if(password === password2) {
       if(validatePassword(password)) {
-        console.log("Username: " + username);
-        console.log("Password: " + password);
         digestMessage(username).then((username_digestHex) => console.log("Hashed Username: ", username_digestHex));
         digestMessage(password).then((password_digestHex) => console.log("Hashed Password: ", password_digestHex));
+        throw new Error('Password created succesfully!')
+      } 
+      else {
+        throw new Error('Oops! Password is not secure enough!');
       }
     }
     else {
-      console.log("Passwords do not match!");
+      throw new Error('Oops! Passwords do not match!');
     }
 }
 
 function App() {
+  const [statusMessage, setStatusMessage] = useState(null);
+
+  const handleButtonClick = (event) => {
+    event.preventDefault(); // REMOVES PAGE REFRESH ON BUTTON CLICK
+    try {
+      register_function();
+    } catch (status) {
+      setStatusMessage(status.message);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -45,7 +79,7 @@ function App() {
           <ul>
             <li>At least 5 characters.</li>
             <li>At least 1 upper-case character.</li>
-            <li>At least 1 numerical character.</li>
+            <li>At least 1 integer.</li>
           </ul>
           <label for="username">Username:</label>
           <input type="text" id="username" name="username" placeholder="Enter your username"></input>
@@ -53,7 +87,8 @@ function App() {
           <input type="password" id="password" name="password" placeholder="Enter your password"></input>
           <label for="confirm_password">Confirm Password:</label>
           <input type="password" id="password2" name="password2" placeholder="Re-enter your password"></input>
-          <button className = "register-button" onClick={register_function}> Register </button>
+          <button className = "register-button" onClick={handleButtonClick}> Register </button>
+          {statusMessage && <div className='status-message'>{statusMessage}</div>}
         </form> 
       </header>
     </div>
